f70289deb5587884c7c6a092890d201eecff18f9
blaec <odeskonst@yandex.ru>
2020-08-16 00:09:03 +0300

    movie card restyling

--- src/main/java/org/blaec/movies/AppMain.java
+++ src/main/java/org/blaec/movies/AppMain.java
@@ -34,7 +34,7 @@ public class AppMain {
 //        LoadMovies.getMoviesFromFolder(VIDEOS).forEach(System.out::println);
 //        LoadMovies.getMoviesFromFolder(CARTOONS).forEach(System.out::println);
 
-        List<MovieFileObject> folderMovies = FilesUtils.getMoviesFromFolder(MOVIES);
+        List<MovieFileObject> folderMovies = FilesUtils.getMoviesFromFolder(CARTOONS);
         MovieDao dao = DBIProvider.getDao(MovieDao.class);
         List<MovieDbObject> dbMovies = dao.getAll();
         for (MovieFileObject movieFile : folderMovies) {
