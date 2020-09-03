71f200f9e244f1dd91e7707a285dabf8dafd8256
blaec <odeskonst@yandex.ru>
2020-09-03 23:36:57 +0300

    #18 add config with movie locations

--- src/main/java/org/blaec/movies/configs/MovieConfig.java
+++ src/main/java/org/blaec/movies/configs/MovieConfig.java
@@ -8,20 +8,29 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MovieConfig {
-    private static final MovieConfig INSTANCE = new MovieConfig(Configs.getConfig("movie.conf", "omdbapi"));
+    private static final MovieConfig INSTANCE = new MovieConfig(Configs.getConfig("movie.conf"));
 
     final private String endpoint;
     final private String paramTitle;
     final private String paramYear;
     final private String paramApikey;
     final private String valueApikey;
+    final private String linkMovies;
+    final private String linkCartoons;
+    final private String linkVideos;
+    final private String linkSerialMovies;
+
 
     private MovieConfig(Config conf) {
-        endpoint = conf.getString("endpoint");
-        paramTitle = conf.getString("paramName.title");
-        paramYear = conf.getString("paramName.year");
-        paramApikey = conf.getString("paramName.apikey");
-        valueApikey = conf.getString("paramValue.apikey");
+        endpoint = conf.getString("omdbapi.endpoint");
+        paramTitle = conf.getString("omdbapi.paramName.title");
+        paramYear = conf.getString("omdbapi.paramName.year");
+        paramApikey = conf.getString("omdbapi.paramName.apikey");
+        valueApikey = conf.getString("omdbapi.paramValue.apikey");
+        linkMovies = conf.getString("upload.movies");
+        linkCartoons = conf.getString("upload.cartoons");
+        linkVideos = conf.getString("upload.videos");
+        linkSerialMovies = conf.getString("upload.serialMovies");
     }
 
     /**
@@ -40,6 +49,20 @@ public class MovieConfig {
     }
 
     /**
+     * Get location with movie files
+     *
+     * @return map with key - for location description and value - for path to file location
+     */
+    public static Map<String, String> getFileLocations() {
+        return ImmutableMap.of(
+                "Movies", INSTANCE.linkMovies,
+                "Cartoons", INSTANCE.linkCartoons,
+                "Videos", INSTANCE.linkVideos,
+                "Serial Movies", INSTANCE.linkSerialMovies
+        );
+    }
+
+    /**
      * Convert map with key-value parameters into string like key1=value1&key2=value2...
      *
      * @param params map with key-value parameters
@@ -51,14 +74,4 @@ public class MovieConfig {
                 .collect(Collectors.joining("&"));
     }
 
-    @Override
-    public String toString() {
-        return "MovieConfig{" +
-                "endpoint='" + endpoint + '\'' +
-                ", paramTitle='" + paramTitle + '\'' +
-                ", paramYear='" + paramYear + '\'' +
-                ", paramApikey='" + paramApikey + '\'' +
-                ", valueApikey='" + valueApikey + '\'' +
-                '}';
-    }
 }
