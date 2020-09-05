99164ccdd85255f1d77cbe459efef06a3cec7992
blaec <odeskonst@yandex.ru>
2020-09-05 16:35:16 +0300

    #21 redirect to downloaded card

--- src/main/webapp/js/upload.js
+++ src/main/webapp/js/upload.js
@@ -2,6 +2,9 @@ let loc = "upload";
 activateMenu(loc);
 
 $(document).ready(() => {
-
+        $(".list-group-item-success").on('click', (event) => {
+            let movieIds = event.currentTarget.textContent.split(" <---> ")[1].trim();
+            window.location.href = `gallery?input-title=${movieIds}&selected-genre=Choose+any+value&selected-actor=Choose+any+value`;
+        })
     }
 );
