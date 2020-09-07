b5cf474e0811edc4d8f930d58628621afd94c05a
blaec <odeskonst@yandex.ru>
2020-09-07 23:36:59 +0300

    #17 show icons with events after events inits

--- src/main/webapp/js/gallery.js
+++ src/main/webapp/js/gallery.js
@@ -2,26 +2,31 @@ let loc = "gallery";
 activateMenu(loc);
 
 $(document).ready(() => {
+    let movieDelete = $(".movie-delete");
+    let movieSync = $(".movie-sync");
 
-        // ======================== listeners ========================
-        // delete movie
-        $(".movie-delete").on('click', (event) => {
-            let movieIds = $(event.currentTarget).parents(".flip-container").children("input");
-            let id = movieIds.attr("value");
-            let imdbId = movieIds.attr("id");
-            let title = $(event.currentTarget).parents(".movie-details").children(".movie-title").text();
-            if (confirm(`Do you really want to delete movie '${title}'?`)) {
-                $.ajax({
-                    url: `gallery?action=delete&id=${id}&imdbId=${imdbId}`,
-                    type: "GET"
-                }).done(function () {
-                    location.reload();
-                });
-            }
-        });
-        $(".movie-sync").on('click', (event) => {
-            alert("sync not implemented yet.")
-        });
-    }
-);
+    // ======================== listeners ========================
+    // delete movie
+    movieDelete.on('click', (event) => {
+        let movieIds = $(event.currentTarget).parents(".flip-container").children("input");
+        let id = movieIds.attr("value");
+        let imdbId = movieIds.attr("id");
+        let title = $(event.currentTarget).parents(".movie-details").children(".movie-title").text();
+        if (confirm(`Do you really want to delete movie '${title}'?`)) {
+            $.ajax({
+                url: `gallery?action=delete&id=${id}&imdbId=${imdbId}`,
+                type: "GET"
+            }).done(function () {
+                location.reload();
+            });
+        }
+    });
+    movieSync.on('click', (event) => {
+        alert("sync not implemented yet.")
+    });
+
+    // show hidden elements after initializing events
+    movieDelete.show();
+    movieSync.show();
+});
 
