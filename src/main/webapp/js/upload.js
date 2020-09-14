99151d619306588b622e65624d79c9d9a494d99a
blaec <odeskonst@yandex.ru>
2020-09-14 22:58:48 +0300

    #23 commenting

--- src/main/webapp/js/upload.js
+++ src/main/webapp/js/upload.js
@@ -2,11 +2,15 @@ let loc = "upload";
 activateMenu(loc);
 
 $(document).ready(() => {
+
+    // ======================== listeners ========================
+    // display successfully loaded movie
     $(".list-group-item-success").on('click', (event) => {
         let movieIds = $(event.currentTarget).attr("value");
         window.location.href = `gallery?input-title=${movieIds}`;
     });
 
+    // display spinner on upload and disable upload button
     $( "form" ).on('submit', (form) => {
         $(".loader").removeClass("loader-hidden");
         $("#btn-upload").attr("disabled", true);
