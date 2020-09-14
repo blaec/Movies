3ef05aff5a2a7de6430eeea93285c7c61a4dd80b
blaec <odeskonst@yandex.ru>
2020-09-14 22:43:15 +0300

    #23 add spinner on upload

--- src/main/webapp/js/upload.js
+++ src/main/webapp/js/upload.js
@@ -5,5 +5,11 @@ $(document).ready(() => {
     $(".list-group-item-success").on('click', (event) => {
         let movieIds = $(event.currentTarget).attr("value");
         window.location.href = `gallery?input-title=${movieIds}`;
-    })
+    });
 });
+
+setLoader = (form) => {
+    $(".loader").removeClass("loader-hidden");
+    form.btnUpload.disabled = true;
+    return true;
+}
