bf1dd9bd276c0ea2b6cd66a9f730e807649fcdf7
blaec <odeskonst@yandex.ru>
2020-09-20 22:43:02 +0300

    extract_card add scripts to wishlist

--- src/main/webapp/js/wishlist.js
+++ src/main/webapp/js/wishlist.js
@@ -2,5 +2,17 @@ let loc = "wishlist";
 activateMenu(loc);
 
 $(document).ready(() => {
+    let movieDelete = $(".movie-delete");
+    let movieSync = $(".movie-sync");
 
+    movieDelete.on('click', (event) => {
+        alert("delete not implemented yet.")
+    });
+    movieSync.on('click', (event) => {
+        alert("sync not implemented yet.")
+    });
+
+    // show hidden elements after initializing events
+    movieDelete.show();
+    movieSync.show();
 });
